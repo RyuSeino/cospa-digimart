ba3437a12e58353d8f7f6c5509753e46ef03379e
Ryu Seino <huntinghighandlow.rs@gmail.com>
2017-12-13 15:31:45 +0900

    use crawlspider

--- digimart_new_items/spiders/digimart.py
+++ digimart_new_items/spiders/digimart.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 from digimart_new_items import items
 
 
-class DigimartSpider(scrapy.Spider):
+class DigimartSpider(CrawlSpider):
     name = 'digimart'
     allowed_domains = ['digimart.net']
 
     start_urls = [
-        'https://www.digimart.net/search?dispMode=ALL&shopNo=&keywordOr=&keywordPhrase=&productName=&nosalep=false&noshopsalep=false&officialCouponShopP=false&categoryId=&categoryNames=&category12Id=101&category3Id=&brandnames=&brandnames=&brandnames=&keywordAnd=&areaId=&priceFrom=&priceTo=&productTypes=NEW_SALE&productTypes=SALE&nosoldoutp=on&x=16&y=11&manufactureYearFrom=&manufactureYearTo=&weightOptionFrom=&weightOptionTo=&term=&stringsoption=&pickupOption=&pickupComponentOption=&otherOption=&fretOption=&neckScaleOption=&bodyOption=&tremolantOption=&fingerboardOption=&neckjointOption=&neckOption=&topMaterialOption=&sideMaterialOption=&backMaterialOption=&bodysizeOption=&bodyShapeOption=&materialOption=&keywordNot=']
+        'https://www.digimart.net/search?dispMode=ALL&nosalep=false&category12Id=101&category3Id=&productTypes=NEW_SALE&productTypes=SALE&nosoldoutp=on']
+
+    rules = [
+        Rule(LinkExtractor(r'/search\?.+NEW_SALE'), callback='parse_item', follow=False)
+    ]
 
 
-
-    def parse(self, response):
-
+    def parse_item(self, response):
         for sel in response.css('div.itemSearchBlock'):
             item = items.DigimartNewItemsItem()
             item['name'] = sel.css('div.itemSearchBox > p.ttl > a::text').extract_first()
             item['price'] = sel.css('p.price::text').extract_first()
             yield item
-
-        sel = response.css('ul.pagerList').xpath("//a[contains(.//text(), '次')]/@href").extract_first()
-        url = response.urljoin(sel)
-        yield scrapy.Request(url, callback=self.parse)
-
-
 
